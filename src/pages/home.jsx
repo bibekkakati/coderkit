158d047e7d7926c661794b093b2e4f718144bf3d
Bibek Kakati <bibekkakati0@gmail.com>
2023-11-03 05:00:03 +0530

    hero section image fixes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,11 +9,11 @@ export default function HomePage() {
 
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row w-1/2">
+            <div className="hero-content flex-row w-1/2 gap-10">
                 <img
-                    src="/android-chrome-192x192.png"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                    loading="eager"
+                    src="/android-chrome-512x512.png"
+                    className="h-[220px] w-[220px]"
+                    alt="CoderKit"
                 />
                 <div>
                     <h1 className="text-5xl font-bold">Hello, Coders!</h1>
